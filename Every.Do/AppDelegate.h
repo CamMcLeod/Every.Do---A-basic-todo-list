a0649da9647e1c4834b6faafe06ed17c639e0e1f
Cam McLeod <cammcleod3@gmail.com>
2019-06-11 11:23:18 -0700

    Initial Commit

---
+++ Every.Do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.Do
+//
+//  Created by Cameron Mcleod on 2019-06-11.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Every.Do/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Every.Do
+//
+//  Created by Cameron Mcleod on 2019-06-11.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
